bd60387505775372f446b8721a1e62fe6a3e084e
VrajParekh <vrajparekh58@gmail.com>
2023-06-26 11:58:08 +0530

    complete

---
+++ src/components/Api.js
@@ -0,0 +1,184 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "Maggi",
+    category: "Breakfast",
+    price: "12₹",
+    description:
+      "Maggi is a popular instant noodle brand loved by many. It is a quick and easy-to-make breakfast option. The noodles are cooked in a flavorful masala broth, making it a delicious and comforting meal.",
+  },
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "Allu Pakoida",
+    category: "Evening",
+    price: "20₹",
+    description:
+      "Allu Pakoida is a crispy and spicy Indian snack made from sliced potatoes coated in a chickpea flour batter and deep-fried until golden brown. It is a perfect tea-time snack enjoyed by many.",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "Corn",
+    category: "Breakfast",
+    price: "10₹",
+    description:
+      "Corn is a nutritious and versatile grain. It can be enjoyed in various forms, such as boiled corn on the cob, roasted corn kernels, or added to salads and soups. It adds a sweet and crunchy element to any meal.",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "Chola",
+    category: "Lunch",
+    price: "50₹",
+    description:
+      "Chola, also known as Chana Masala, is a popular North Indian dish made with chickpeas cooked in a spicy and tangy tomato-based gravy. It is often served with rice, naan, or roti for a satisfying and flavorful meal.",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "Pizza",
+    category: "Evening",
+    price: "80₹",
+    description:
+      "Pizza is a beloved Italian dish that has gained worldwide popularity. It consists of a round, flatbread base topped with a variety of ingredients such as cheese, sauce, vegetables, and meats. It is baked until the crust is crispy and the cheese is melted and gooey.",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "Dinner",
+    price: "180₹",
+    description:
+      "Non-Veg Thali is a complete meal that includes a variety of non-vegetarian dishes like chicken curry, mutton curry, or fish curry along with rice, roti, dal, raita, and a selection of side dishes. It offers a delightful combination of flavors and textures.",
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "Dinner",
+    price: "60₹",
+    description:
+      "Sweets are delightful treats enjoyed after a meal or on special occasions. They come in a variety of forms, such as traditional Indian sweets like Gulab Jamun, Rasgulla, or popular desserts like cakes, pastries, and ice creams.",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "Lunch",
+    price: "60₹",
+    description:
+      "Rajma Rice is a popular Punjabi dish that combines red kidney beans cooked in a thick, flavorful gravy with steamed basmati rice. It is a hearty and nutritious meal enjoyed by many.",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "Samosa",
+    category: "Evening",
+    price: "10₹",
+    description:
+      "Samosa is a popular Indian snack consisting of a crispy, triangular pastry filled with spiced potatoes, peas, and sometimes meat. It is a delicious and savory treat often served with chutney or ketchup.",
+  },
+  {
+    id: 10,
+    image: "../images/dosa.jpg",
+    name: "Dosa",
+    category: "Breakfast",
+    price: "30₹",
+    description:
+      "Dosa is a popular South Indian dish made from fermented rice and lentil batter. It is a thin, crispy pancake usually served with coconut chutney and sambar. Dosa comes in various flavors and fillings, such as masala dosa, cheese dosa, or onion dosa.",
+  },
+  {
+    id: 11,
+    image: "../images/sandwich.jpg",
+    name: "Sandwich",
+    category: "Breakfast",
+    price: "40₹",
+    description:
+      "Sandwiches are a versatile and convenient breakfast option. They consist of various ingredients, such as sliced bread, vegetables, cheese, and spreads, layered together to create a delicious and filling meal. Popular types include grilled cheese sandwich, veggie sandwich, or club sandwich.",
+  },
+  {
+    id: 12,
+    image: "../images/biryani.webp",
+    name: "Biryani",
+    category: "Lunch",
+    price: "120₹",
+    description:
+      "Biryani is a flavorful and aromatic rice dish made with long-grain basmati rice, spices, and meat or vegetables. It is a popular dish in Indian and Middle Eastern cuisine, known for its rich flavors and combination of spices. Varieties include chicken biryani, vegetable biryani, or mutton biryani.",
+  },
+  {
+    id: 13,
+    image: "../images/pasta.avif",
+    name: "Pasta",
+    category: "Dinner",
+    price: "70₹",
+    description:
+      "Pasta is a staple Italian dish made from durum wheat flour and water, formed into various shapes. It is usually boiled and then combined with a sauce, such as tomato sauce, Alfredo sauce, or pesto. Pasta dishes can be customized with different toppings and ingredients.",
+  },
+  {
+    id: 14,
+    image: "../images/idli.jpg",
+    name: "Idli",
+    category: "Breakfast",
+    price: "25₹",
+    description:
+      "Idli is a traditional South Indian steamed rice cake made from fermented batter. It is a popular breakfast option and is often served with coconut chutney and sambar. Idli is soft, fluffy, and easily digestible.",
+  },
+  {
+    id: 15,
+    image: "../images/burger.jpg",
+    name: "Burger",
+    category: "Evening",
+    price: "50₹",
+    description:
+      "Burgers are a classic fast food item loved worldwide. They consist of a grilled or fried patty made from meat, vegetables, or plant-based ingredients, sandwiched between two buns. Burgers can be customized with various toppings, sauces, and condiments.",
+  },
+  {
+    id: 16,
+    image: "../images/pulao.jpg",
+    name: "Pulao",
+    category: "Lunch",
+    price: "80₹",
+    description:
+      "Pulao is a fragrant rice dish cooked with basmati rice, aromatic spices, and a mix of vegetables or meat. It is a flavorful and one-pot meal enjoyed in many cultures. Varieties include vegetable pulao, chicken pulao, or biryani pulao.",
+  },
+  {
+    id: 17,
+    image: "../images/icecream.jpg",
+    name: "Ice Cream",
+    category: "Dessert",
+    price: "35₹",
+    description:
+      "Ice cream is a beloved frozen dessert enjoyed by people of all ages. It comes in a variety of flavors, such as vanilla, chocolate, strawberry, or mint. Ice cream can be served in cones, cups, or paired with other desserts to create delightful combinations.",
+  },
+  {
+    id: 18,
+    image: "../images/pavbhaji.jpg",
+    name: "Pav Bhaji",
+    category: "Evening",
+    price: "60₹",
+    description:
+      "Pav Bhaji is a popular street food dish from Maharashtra, India. It consists of a spicy vegetable curry (bhaji) served with buttered and toasted bread rolls (pav). It is a flavorful and filling snack enjoyed by many.",
+  },
+  {
+    id: 19,
+    image: "../images/momo.jpg",
+    name: "Momos",
+    category: "Evening",
+    price: "40₹",
+    description:
+      "Momos are delicious Tibetan dumplings filled with a choice of meat or vegetable fillings. These steamed dumplings are served with a tangy and spicy dipping sauce. A popular street food snack, momos are loved for their flavorful fillings and satisfying texture.",
+  },
+  {
+    id: 20,
+    image: "../images/rotiwithmeat.jpg",
+    name: "Roti with Meat",
+    category: "Dinner",
+    price: "90₹",
+    description:
+      "Roti with Meat is a flavorful and hearty dish that combines traditional Indian flatbread (roti) with tender and succulent meat. The meat, usually chicken or lamb, is marinated in a blend of aromatic spices and cooked until tender. It is then served with freshly made roti",
+  },
+];
+
+export default Menu;
